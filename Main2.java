351fc6eae7b54cff5fda1e096b7644da9e254763
OmniaAhmed292 <omnyaahmed292@gmail.com>
2022-05-27 14:52:08 +0200

    Remove class Api

--- Main2.java
+++ Main2.java
@@ -24,7 +24,7 @@ public class Main {
         //we need an object to iterate around the sheet we have
         Iterator<Row> iterator = firstSheet.iterator();
         //we make an array list for fields and other for parents
-        ArrayList <Api> elements = new ArrayList<>();
+        ArrayList <Field> elements = new ArrayList<>();
 
         //While iterator still has rows we will iterate on them
         while (iterator.hasNext()) {
@@ -87,35 +87,21 @@ public class Main {
 
 
 
-
-class Api {
-    private String Api_name;
+class Field {
     private String Field_name=null;
     private String allowed_value=null;
     private String mandatory=null;
     private String type=null;
+    private String myParent="No parents"; //here we put the parent of our field (there might be none) initial value
+
     //No arg constructor
-    public  Api(){}
-    //arg constructors
-    public Api(String api_name) {
-        Api_name = api_name;
+    public Field(){
     }
+    public  Field(String field_name, String allowed_value, String mandatory, String type)
+    {
 
-    public Api(String field_name, String allowed_value, String mandatory, String type) {
-        Field_name = field_name;
-        this.allowed_value = allowed_value;
-        this.mandatory = mandatory;
-        this.type = type;
     }
-
-    public String getApi_name() {
-        return Api_name;
-    }
-
-    public void setApi_name(String api_name) {
-        Api_name = api_name;
-    }
-
+    //Setters and getters
     public String getField_name() {
         return Field_name;
     }
@@ -132,6 +118,10 @@ class Api {
         this.allowed_value = allowed_value;
     }
 
+    public void setMyParent(String myParent) {
+        this.myParent = myParent;
+    }
+
     public String getMandatory() {
         return mandatory;
     }
@@ -143,63 +133,9 @@ class Api {
     public String getType() {
         return type;
     }
-    public void getParent(){};
+
     public void setType(String type) {
         this.type = type;
-    }
-    public void Print(){
-
-    }
-}
-
-
-
-class Field extends Api {
-    private String myParent="No parents"; //here we put the parent of our field (there might be none) initial value
-
-    //No arg constructor
-    public Field(){
-        super();
-    }
-    public  Field(String field_name, String allowed_value, String mandatory, String type)
-    {
-        super(field_name,allowed_value,mandatory,type);
-    }
-
-    public void setField_name(String field_name) {
-        super.setField_name(field_name);
-    }
-
-    public void setAllowed_value(String allowed_value) {
-        super.setAllowed_value(allowed_value);
-    }
-
-    public void setMandatory(String mandatory) {
-        super.setMandatory(mandatory);
-    }
-    public void setType(String type) {
-        super.setType(type);
-    }
-
-    public void setParent(String parent) {
-        this.myParent = parent;
-    }
-
-    public String getField_name() {
-
-        return super.getField_name();
-    }
-
-    public String getAllowed_value() {
-        return super.getAllowed_value();
-    }
-
-    public String getMandatory() {
-        return super.getMandatory();
-    }
-
-    public String getType() {
-        return super.getType();
     }
 
     public String getMyParent() {
@@ -209,20 +145,20 @@ class Field extends Api {
 
     //return true of the field has no parents and false if not
     public boolean hasNOParents(){
-        return super.getField_name().startsWith("/field"); //if there's no object before the field name
+        return this.getField_name().startsWith("/field"); //if there's no object before the field name
     }
 
     public void getParent(){//this function should return the name of the fields parent ******Needs Check*******
         String [] arr;
-        arr= super.getField_name().split("/"); //Split /object1/object2/field6 into [ ,object1, object2,field6]
-        if(this.hasNOParents()) super.setField_name(arr[arr.length-1]);
+        arr= this.getField_name().split("/"); //Split /object1/object2/field6 into [ ,object1, object2,field6]
+        if(arr.length<=2) {this.setField_name(arr[arr.length-1]);}
         else{
             setField_name(arr[arr.length-1]); //Update Field name
-            setParent(arr[arr.length-2]); }//the name before field name is the parent
+            setMyParent(arr[arr.length-2]); }//the name before field name is the parent
         //We need to connect this parent to an object we already made not make a new one
 
     }
-    @Override
+    //Print function
     public void Print(){
         System.out.println("Field name is "+ getField_name());
         System.out.println("Allowed value is"+ getAllowed_value());
@@ -234,9 +170,9 @@ class Field extends Api {
 
 } //End of Field Class
 
-class Parent extends Api {
+class Parent extends Field {
     //Data fields
-    private String myParent="No parents"; //initial value
+
     private String [] children;
     //no arg constructor
     public Parent(){
@@ -254,59 +190,8 @@ class Parent extends Api {
     public void setChildren(String[] children) {
         this.children = children;
     }
-    public void setField_name(String field_name) {
-
-        super.setField_name(field_name);
-    }
-
-    public void setAllowed_value(String allowed_value) {
-        super.setAllowed_value(allowed_value);
-    }
-
-    public void setMandatory(String mandatory) {
-        super.setMandatory(mandatory);
-    }
-    public void setType(String type) {
-        super.setType(type);
-    }
-
-    public void setParent(String parent) {
-        this.myParent = parent;
-    }
-
-    public String getField_name() {
-        return super.getField_name();
-    }
-
-    public String getAllowed_value() {
-        return super.getAllowed_value();
-    }
-
-    public String getMandatory() {
-        return super.getMandatory();
-    }
-
-    public String getType() {
-        return super.getType();
-    }
-
-    public String getMyParent() {
-        getParent();
-        return myParent;
-    }
-
-    public void getParent(){//this function should return the name of the fields parent ******Needs Check*******
-        String [] arr;
-        arr= super.getField_name().split("/"); //Split /object1/object2/field6 into [ ,object1, object2,field6]
-        if(arr.length<=2) {super.setField_name(arr[arr.length-1]);}
-        else {
-            super.setField_name(arr[arr.length-1]); //Update Field name
-            setParent(arr[arr.length-2]); }//the name before field name is the parent
-        //We need to connect this parent to an object we already made not make a new one
-
-    }
     @Override
-    public void Print(){
+    public void Print(){ //still need to add children
         System.out.println("Field name is "+ getField_name());
         // System.out.println("Allowed value is"+ getAllowed_value()); no allowed value for objects
         System.out.println("Mandatory is "+getMandatory());
