cf388f2047f4a16cb3191645116307d03f55f825
engRana404 <1901398@eng.asu.edu.eg>
2022-05-28 21:42:17 +0200

    some edits in the main.c file and romeved the the unwanted files

--- Main.java
+++ Main.java
@@ -4,15 +4,24 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 //Apache
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import javafx.application.*;
+import java.awt.*;
+import javafx.scene.layout.*;
 
 
-public class main {
-    public static void main(String[] args) throws IOException {
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws IOException{
         int skippedRows=0; //we need them because some rows with no useful info are skipped
 
         //Create a file with our wanted path
@@ -35,11 +44,6 @@ public class main {
             //we will iterate on the cells of each row
             Iterator<Cell> cellIterator = nextRow.cellIterator();
 
-            /*if(nextRow.getRowNum()<6){
-            skippedRows++;
-            continue;
-            }*/
-
             //we had to do this due to the empty rows in the middle
             try{//If the type is string put in fields else put in parent
                 if(nextRow.getCell(2).getStringCellValue().equals("string")){elements.add(new Field());}
@@ -49,7 +53,7 @@ public class main {
             }
             catch (NullPointerException e)
             {
-                //Sometimes the row is completely empty and thus it's trated as null and cause error
+                //Sometimes the row is completely empty, and thus it's treated as null and cause error
                 //We have no choice but to skip it.
                 skippedRows++;
                 continue;
@@ -85,28 +89,66 @@ public class main {
             } //End of the default Cell iterator
         } //End of the Row iterator
 
-        //For Gui we made an array of parents only, because we will display them
-        ArrayList <Field> parents=new ArrayList<>();
+        //For Gui, we made an array of parents only, because we will display them
+        ArrayList <Field> parents = new ArrayList<>();
         for(Field f : elements){
             if(f instanceof Parent) parents.add(f);
             else if(f.hasNOParents()){ parents.add(f);}
         }
 
-
-            /*ATestbench
-            for(Field p: parents){
-               // p.Print();
-            System.out.println("Children are : "+p.PutChildren(elements));
-            System.out.println("--");
-
-            }*/
-
         //Close WorkBook and the whole file
         workbook.close();
         fis.close();
-    } //End of main function
-} //End of main class
+        try {
+            //Arraylist<parent> p=new Arraylist<parent>();
+            ArrayList<VBox> root = new ArrayList<>() ;
+            ArrayList<Scene> scene = new ArrayList<>();
+            ArrayList<Stage> stage = new ArrayList<>();
+            ArrayList <Text> Headline1 = new ArrayList<>();
+            ArrayList <Text> Headline2 = new ArrayList<>();
+            ArrayList<Pane> pane = new ArrayList<>();
+            for(int i = 0; i<parents.size(); i++) //obj loop
+            {
+                root.add(new VBox());
+                scene.add(new Scene(root.get(i),300,300));
+                pane.add(new Pane());
+                Headline1.add(new Text());
+                Headline2.add(new Text());
+                //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+                stage.add(new Stage());
+                stage.get(i).setTitle(parents.get(i).getField_name());
+                stage.get(i).setScene(scene.get(i));
+                String b = parents.get(i).PutChildren(elements);
 
+                Font font =new Font("Georgia", Font.BOLD,20);
+
+                Headline1.get(i).setText(parents.get(i).getField_name());
+                Headline2.get(i).setText(b);
+                Headline1.get(i).setFont(new javafx.scene.text.Font("Georgia",20));
+                Headline1.get(i).setFont(new javafx.scene.text.Font("Times new roman",16));
+                //Headline1.get(i).fontProperty();
+
+
+
+
+                root.get(i).getChildren().add(Headline1.get(i));
+                root.get(i).getChildren().add(Headline2.get(i));
+
+                //root.get(i).getChildren().add();
+                stage.get(i).show();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        //End of main class
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
 
 
 
